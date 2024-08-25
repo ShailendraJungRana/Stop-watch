b02304d095aee2cc2dcb50beaaf43efbdad6fa4d
Arun jR <cannon.arun1234@gmail.com>
2024-08-25 14:56:23 +0545

    Stop-Watch Lets get Started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App text-3xl  ">
+     Hello
+    </div>
+  );
+} 
+
+export default App;
---
+++ src/component/StopWatch.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+import "./App.css"
+function StopWatch() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StopWatch
